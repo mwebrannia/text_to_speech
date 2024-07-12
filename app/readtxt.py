dab1fe7ee2b21188a576a2feeb1443bc8081e794
Johannes JPL <100988085+mwebrannia@users.noreply.github.com>
2024-07-12 14:45:00 +0300

    Update readtxt.py
    txt-speech_v1.0

--- app/readtxt.py
+++ app/readtxt.py
@@ -4,19 +4,20 @@ def speak_text(file_path, voice_gender='female', rate=200):
   try:
     engine = pyttsx3.init()
 
-    # Set the voice gender
+    #voice gender
     voices = engine.getProperty('voices')
     if voice_gender == 'female':
         engine.setProperty('voice', voices[1].id)  # index 1 is female voice
     else:
         engine.setProperty('voice', voices[0].id)  # index 0 is male voice
         
-    # Set the speech rate
+    #speech rate
     engine.setProperty('rate', rate)
 
     with open(file_path, 'r') as file:
       text = file.read()
 
+      #using the engine to say(speak) text and then stop after finishing.
       engine.say(text)
       engine.runAndWait()
       engine.stop()
