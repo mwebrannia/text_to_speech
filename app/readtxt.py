3322127bd33da4ceced86f10a3c53b8d1751c1fa
Johannes JPL <100988085+mwebrannia@users.noreply.github.com>
2024-07-12 14:38:54 +0300

    Add files via upload
    txt-speech_v1

---
+++ app/readtxt.py
@@ -0,0 +1,30 @@
+import pyttsx3
+
+def speak_text(file_path, voice_gender='female', rate=200):
+  try:
+    engine = pyttsx3.init()
+
+    # Set the voice gender
+    voices = engine.getProperty('voices')
+    if voice_gender == 'female':
+        engine.setProperty('voice', voices[1].id)  # index 1 is female voice
+    else:
+        engine.setProperty('voice', voices[0].id)  # index 0 is male voice
+        
+    # Set the speech rate
+    engine.setProperty('rate', rate)
+
+    with open(file_path, 'r') as file:
+      text = file.read()
+
+      engine.say(text)
+      engine.runAndWait()
+      engine.stop()
+
+  except FileNotFoundError:
+    print(f"The file at {file_path} was not found.")
+  except Exception as e:
+    print(f"An error occured: {e}")
+
+file_path = "py/Apple.txt"
+speak_text(file_path, voice_gender='female', rate=170)
